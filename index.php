afe49dea8c8947596e779e8abeb263e6cc5ee329
Yamert <yamert@Electroclash.local>
2018-07-21 10:18:52 +0300

    Update

--- index.php
+++ index.php
@@ -89,7 +89,7 @@ try {
         $response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
         $response = $client->sendMessage([
                                          'chat_id' => $update->message->chat->id,
-                                         'text' => "Мы в машине.... " + 'chat_id'
+                                         'text' => 'chat_id'
                                          ]);
         
     }
