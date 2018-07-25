fb83532315d13bda28c6c5a7a74f78afd61414a6
Yamert <yamert@Electroclash.local>
2018-07-26 00:07:31 +0300

    Commit bleat

--- TelegramBot.php
+++ TelegramBot.php
@@ -1,7 +1,7 @@
 <?php
 
 
-use vendor\GuzzleHttp\guzzle\src\Client;
+$client = new Zelenin\Telegram\Bot\Api('520672444:AAF2z3IJXUPUJ7si1Bdw6N8D2Ejcjq-B7lA');
 
 
 class TelegramBot
@@ -16,12 +16,13 @@ class TelegramBot
             if (!empty($params)) {
                $url .= "?" . http_build_query($params);
             }
-
+/*
             $client = new Client([
                 'base_uri' => $url
             ]);
             $result = $client->request('GET');
             return json_decode($result->getBody());
+*/
         }
 
 
