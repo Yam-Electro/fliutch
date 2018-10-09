72183dcc261f742af6f91b52598ada2a3d592dea
Yamert <yamert@Electroclash.local>
2018-10-10 00:08:38 +0300

    make it better

--- index.php
+++ index.php
@@ -190,10 +190,10 @@ $rawXML = str_replace(']]>', "", $rawXML);
     {   $html=simplexml_load_file('https://bash.im/rss/');
         $pp = "\n";
         $counter = 0;
-        $randomcounter = rand(2, 30);
+        //$randomcounter = rand(2, 30);
         foreach ($html->channel->item as $item)
         {
-
+            $randomcounter = rand(2, 30);
             $counter++;
             if($counter = $randomcounter)
             {
