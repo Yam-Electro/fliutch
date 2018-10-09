9280849f45aca17db073b5b5f8d36bbf8e14a3a7
Yamert <yamert@Electroclash.local>
2018-10-09 23:24:47 +0300

    make it better

--- index.php
+++ index.php
@@ -16,12 +16,171 @@ try {
 
     if($update->message->text == '/email')
     {
-    	$response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
-    	$response = $client->sendMessage([
-        	'chat_id' => $update->message->chat->id,
-        	'text' => "Можете писать этому типу (а можете не писать111): optixkiller@gmail.com"
-     	]);
+        $response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
+        $response = $client->sendMessage([
+            'chat_id' => $update->message->chat->id,
+            'text' => "Можете писать этому типу (а можете не писать111): optixkiller@gmail.com"
+        ]);
     }
+
+    else if($update->message->text == 'упоротое животное')
+    {
+        //$response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
+        $response = $client->sendMessage([
+            'chat_id' => $update->message->chat->id,
+            'text' => "Ты разговариваешь с собственным ботом, кто бы, блять, говорил."
+        ]);
+    }
+
+    else if($update->message->text == 'вейперы')
+    {
+        $response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
+        $response = $client->sendMessage([
+            'chat_id' => $update->message->chat->id,
+            'text' => "Вейперы - это такая разновидность гомосексуалистов или я просто не в тренде ? "
+        ]);
+    }
+
+    else if($update->message->text == 'вейп')
+    {
+        $response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
+        $response = $client->sendMessage([
+            'chat_id' => $update->message->chat->id,
+            'text' => "Это что-то из секс шопа? "
+        ]);
+    }
+
+
+    else if($update->message->text == 'намотка')
+    {
+        $response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
+        $response = $client->sendMessage([
+            'chat_id' => $update->message->chat->id,
+            'text' => "Иди суда, я тебе кантал на *** намотаю!"
+        ]);
+    }
+
+    else if($update->message->text == 'Эрхог')
+    {
+        $response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
+        $response = $client->sendMessage([
+            'chat_id' => $update->message->chat->id,
+            'text' => "Это твой друг вейпер?"
+        ]);
+    }
+
+    else if($update->message->text == 'мыш')
+    {
+        $response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
+        $response = $client->sendMessage([
+            'chat_id' => $update->message->chat->id,
+            'text' => "кродеться"
+        ]);
+    }
+
+
+
+    else if($update->message->text == 'да')
+    {
+        $response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
+        $response = $client->sendMessage([
+            'chat_id' => $update->message->chat->id,
+            'text' => "джигурда!"
+        ]);
+    }
+
+    else if($update->message->text == 'Серега')
+    {
+        $response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
+        $response = $client->sendMessage([
+            'chat_id' => $update->message->chat->id,
+            'text' => "серёга (от лат. yobaniy v rot, eto cho, nature ty? sergunya, seriy, serhio, serginio, serious serik, sergofan, serovodorod, serg, sergey) — сколько лет, сколько зим, как жизнь?"
+        ]);
+    }
+
+
+
+
+    /*
+     *        }elseif ($text == "Картинка") {
+            $url = "https://68.media.tumblr.com/6d830b4f2c455f9cb6cd4ebe5011d2b8/tumblr_oj49kevkUz1v4bb1no1_500.jpg";
+            $telegram->sendPhoto([ 'chat_id' => $chat_id, 'photo' => $url, 'caption' => "Описание." ]);
+     *
+     */
+
+
+    else if($update->message->text == 'мыш')
+    {   $url = "https://vk.com/photo21506599_456239218";
+        $response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
+        $response = $client->sendPhoto([
+
+            'chat_id' => $update->message->chat->id,
+            'photo' => $url
+        ]);
+    }
+
+
+    else if($update->message->text == '/popyachsa@Yamertbot')
+    {   $url = "https://upyachka.io/img/up4kman.gif";
+        $response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
+        $response = $client->sendDocument([
+
+            'chat_id' => $update->message->chat->id,
+            'document' => $url
+        ]);
+    }
+
+    /*
+     * elseif ($text == "Последние статьи") {
+            $html=simplexml_load_file('http://netology.ru/blog/rss.xml');
+            foreach ($html->channel->item as $item) {
+	     $reply .= "\xE2\x9E\xA1 ".$item->title." (<a href='".$item->link."'>читать</a>)\n";
+        	}
+
+
+            $telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode' => 'HTML', 'disable_web_page_preview' => true, 'text' => $reply ]);
+        }
+     */
+
+    else if($update->message->text == '/statya@Yamertbot')
+    {   $html=simplexml_load_file('http://netology.ru/blog/rss.xml');
+        foreach ($html->channel->item as $item)
+        {
+            $reply .= "\xE2\x9E\xA1 ".$item->title." (<a href='".$item->link."'>читать</a>)\n";
+        }
+
+        $response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
+        $response = $client->sendMessage([
+
+            'chat_id' => $update->message->chat->id, 'parse_mode' => 'HTML', 'disable_web_page_preview' => true, 'text' => $reply
+        ]);
+    }
+
+    /*
+     * $rss =  simplexml_load_file('http://bash.org.ru/rss/');
+        $item = $rss->channel->item;
+        for ($ib=0;$ib<101;$ib++) {
+        $bash_utf=$item[$ib]->description;
+        echo $bash_utf;};
+
+
+        <?php
+        $xml = simplexml_load_file('http://bash.org.ru/rss/');
+        foreach ($xml->xpath('//item') as $item)
+        {
+        echo "<b><a href="$item->guid">$item->title
+        <br/></a></b>$item->description<hr/>";
+        }
+        ?>
+
+    $rawXML = str_replace('<![CDATA[<?xml version="1.0" encoding="UTF-8" ?>', "", $rawXML);
+$rawXML = str_replace(']]>', "", $rawXML);
+
+              //убираем лишнее из текста
+           // $item = str_replace('<![CDATA[', '', $item);
+            //$item = str_replace(']]>', '', $item);
+
+     */
 
     else if($update->message->text == '/bash@Yamertbot')
     {   $html=simplexml_load_file('https://bash.im/rss/');
@@ -41,8 +200,8 @@ try {
             $reply .= $item->description.$pp.$pp;
 
         }
-        $reply = str_replace('<br>', " ", $reply);
-        $reply = str_replace('&quot', " ", $reply);
+        $reply = str_replace('<br>', "", $reply);
+        $reply = str_replace('&quot', "", $reply);
 
         $response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
         $response = $client->sendMessage([
@@ -50,6 +209,168 @@ try {
             'chat_id' => $update->message->chat->id, 'parse_mode' => 'Markdown', 'disable_web_page_preview' => true, 'text' => $reply
         ]);
     }
+
+
+    /*
+        else if($update->message->text == '/bash@Yamertbot')
+        {   $html=simplexml_load_file('https://bash.im/rss/');
+            //$item = $html->channel->item;
+            $pp = "\n";
+            foreach ($html->channel->item as $item)//->description foreach ($html->channel->item->description as $item)
+            {
+                $reply .= $item->title.$pp; //.$item->description.$pp.$pp; //$reply .= $item->title; //>title;
+            }
+
+            $response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
+            $response = $client->sendMessage([
+
+                'chat_id' => $update->message->chat->id, 'parse_mode' => 'HTML', 'disable_web_page_preview' => true, 'text' => $reply
+            ]);
+        }
+    */
+
+    else if($update->message->text == '/weather@Yamertbot')
+    {   $html=simplexml_load_file('http://informer.gismeteo.ru/rss/34929.xml');
+        $pp = "\n";
+        foreach ($html->channel->item as $item)
+        {
+            $reply .= $item->title.$pp.$item->description.$pp.$pp;
+        }
+
+        $response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
+        $response = $client->sendMessage([
+
+            'chat_id' => $update->message->chat->id, 'parse_mode' => 'HTML', 'disable_web_page_preview' => true, 'text' => $reply
+        ]);
+    }
+
+    //news
+    else if($update->message->text == '/news@Yamertbot')
+    {   $html=simplexml_load_file('http://bloknot-krasnodar.ru/rss_news.php');
+        $pp = "\n";
+        foreach ($html->channel->item as $item)
+        {
+            $reply .= $item->title.$pp.$item->description.$pp.$item->link.$pp.$pp;
+        }
+
+        $response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
+        $response = $client->sendMessage([
+
+            'chat_id' => $update->message->chat->id, 'parse_mode' => 'HTML', 'disable_web_page_preview' => true, 'text' => $reply
+        ]);
+    }
+
+    //kino
+    else if($update->message->text == '/cinema@Yamertbot')
+    {   $html=simplexml_load_file('https://st.kp.yandex.net/rss/news_premiers.rss');
+        $pp = "\n";
+        $count = 0;
+        foreach ($html->channel->item as $item)
+        {
+            $counter++;
+            if($counter > 5)
+            {
+                break;
+            }
+            $reply .= $item->title.$pp.$item->link.$pp.$pp;
+        }
+
+        $response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
+        $response = $client->sendMessage([
+
+            'chat_id' => $update->message->chat->id, 'parse_mode' => 'HTML', 'disable_web_page_preview' => true, 'text' => $reply
+        ]);
+    }
+
+    /*
+    //pravovlavie
+    else if($update->message->text == '/omg@Yamertbot')
+    {   $html=simplexml_load_file('http://www.pravoslavie.ru/xml/prav_answers_rss.xml');
+        $pp = "\n";
+        foreach ($html->channel->item as $item)
+        {
+            $reply .= $item->title.$pp.$item->description.$pp.$item->link.$pp.$pp;
+        }
+
+        $response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
+        $response = $client->sendMessage([
+
+            'chat_id' => $update->message->chat->id, 'parse_mode' => 'HTML', 'disable_web_page_preview' => true, 'text' => $reply
+        ]);
+    }
+    */
+
+
+    //pravovlavie
+
+    else if($update->message->text == '/omg@Yamertbot')
+    {   $html=simplexml_load_file('http://www.pravoslavie.ru/xml/prav_answers_rss.xml');
+        $pp = "\n";
+        $count = 0;
+        foreach ($html->channel->item as $item)
+        {
+            $counter++;
+            if($counter > 2)
+            {
+                break;
+            }
+            $reply .= $item->title.$pp.$item->description.$pp.$item->link.$pp.$pp;
+        }
+        $response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
+        $response = $client->sendMessage([
+            'chat_id' => $update->message->chat->id, 'parse_mode' => 'HTML', 'disable_web_page_preview' => true, 'text' => $reply
+        ]);
+    }
+
+
+
+    //http://omskregion.info/rss.xml
+    else if($update->message->text == '/omsk@Yamertbot')
+    {   $html=simplexml_load_file('http://omskregion.info/rss.xml');
+        $pp = "\n";
+        $count = 0;
+        foreach ($html->channel->item as $item)
+        {
+            $reply .= $item->title.$pp.$item->description.$pp.$pp;
+            $counter++;
+            if($counter > 5)
+            {
+                break;
+            }
+
+        }
+        $response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
+        $response = $client->sendMessage([
+            'chat_id' => $update->message->chat->id, 'parse_mode' => 'HTML', 'disable_web_page_preview' => true, 'text' => $reply
+        ]);
+    }
+
+
+
+
+
+    /*
+
+        else if($update->message->text == 'test')
+
+            $keyboard = new  Zelenin\Telegram\Bot\Type\Inline\InlineKeyboardMarkup(   //\TelegramBot\Api\Types\Inline\InlineKeyboardMarkup(
+                [
+                    [
+                        ['callback_data' => 'data_test', 'text' => 'Answer'],
+                        ['callback_data' => 'data_test2', 'text' => 'ОтветЪ']
+                    ]
+                ]
+            );
+
+        {
+            $response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
+            $response = $client->sendMessage([
+                'chat_id' => $update->message->chat->id,
+                'text' => "\xF0\x9F\x98\x81"
+            ]);
+        }
+    */
+
 
     
 } catch (\Zelenin\Telegram\Bot\NotOkException $e) {
