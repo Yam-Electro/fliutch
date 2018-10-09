a2b9009489f96782065d95ee84fcc66799a389cb
Yamert <yamert@Electroclash.local>
2018-10-09 23:01:59 +0300

    make it better

--- index.php
+++ index.php
@@ -23,6 +23,34 @@ try {
      	]);
     }
 
+    else if($update->message->text == '/bash@Yamertbot')
+    {   $html=simplexml_load_file('https://bash.im/rss/');
+        $pp = "\n";
+        $counter = 0;
+        foreach ($html->channel->item as $item)
+        {
+            $counter++;
+            if($counter > 1)
+            {
+                break;
+            }
+            //убираем лишнее из текста
+            //$item = str_replace('<br>', "", $item);
+            //$item = str_replace('&quot', "", $item);
+
+            $reply .= $item->description.$pp.$pp;
+
+        }
+        $reply = str_replace('<br>', "", $reply);
+        $reply = str_replace('&quot', "", $reply);
+
+        $response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
+        $response = $client->sendMessage([
+            // 'chat_id' => $update->message->chat->id, 'parse_mode' => 'Markdown', 'disable_web_page_preview' => true, 'text' => $reply
+            'chat_id' => $update->message->chat->id, 'parse_mode' => 'Markdown', 'disable_web_page_preview' => true, 'text' => $reply
+        ]);
+    }
+
     
 } catch (\Zelenin\Telegram\Bot\NotOkException $e) {
 
